23d13f474419100356f587d879ee9b03874d0906
Ryan Valentine <3958672+ramseylove@users.noreply.github.com>
2022-03-15 11:46:20 -0600

    styled audio component

--- podcast-player/src/components/episode/EpisodePlayer.jsx
+++ podcast-player/src/components/episode/EpisodePlayer.jsx
@@ -1,16 +1,58 @@
 import React from "react";
 
+import { Box, Card, CardMedia, Link, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const PodcastPlayer = styled("audio")(({ theme }) => ({
+  width: "100%",
+  height: "2.0rem",
+}));
+
 function EpisodePlayer({ episode }) {
-  // const { $length, $type, $url } = content;
   return (
-    <div className="player">
-      <audio
-        controls
-        src={episode.enclosure.url}
-        preload="metadata"
-        autoPlay
-      ></audio>
-    </div>
+    <Card
+      sx={{
+        p: ".3rem",
+        width: "40rem",
+        alignSelf: "stretch",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#88737a",
+        color: "primary.contrastText",
+      }}
+    >
+      <Box sx={{ fontSize: ".9rem", fontWeight: "600", mb: ".4rem" }}>
+        {episode.title}
+      </Box>
+      <Box sx={{ fontSize: ".7rem", fontWeight: "700", mb: ".4rem" }}>
+        {episode.pubDate}
+      </Box>
+      <Typography
+        noWrap="true"
+        sx={{ fontSize: ".7rem", mb: ".3rem", mr: "1rem" }}
+      >
+        {episode.description}
+      </Typography>
+      <Box sx={{ fontSize: ".6rem" }}>
+        <Link
+          sx={{
+            fontWeight: "700",
+            color: "primary.contrastText",
+            textDecoration: "underline solid white",
+          }}
+        >
+          Read More
+        </Link>
+      </Box>
+      <CardMedia sx={{ mt: ".3rem", mb: 0 }}>
+        <PodcastPlayer
+          controls
+          src={episode.enclosure.url}
+          preload="metadata"
+        ></PodcastPlayer>
+      </CardMedia>
+    </Card>
   );
 }
 
--- podcast-player/src/components/header.jsx
+++ podcast-player/src/components/header.jsx
@@ -10,59 +10,39 @@ function Header({ selectedEpisodePlaying, setSideBarOpen, sideBarOpen }) {
     setSideBarOpen(sideBarOpen);
   };
   return (
-    <Box
+    <AppBar
+      position="sticky"
       sx={{
         flexGrow: 1,
         bgcolor: "primary.main",
       }}
     >
-      <AppBar position="static">
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            height: "8rem",
-          }}
-        >
-          <Toolbar>
-            <IconButton
-              size="large"
-              color="inherit"
-              aria-label="open sidebar"
-              onClick={toggleDrawer(true)}
-              edge="start"
-              sx={{ mr: 2, ml: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <h1>Ultimate Podcasting</h1>
-          </Toolbar>
-          {selectedEpisodePlaying && (
-            <Box
-              sx={{
-                width: "40rem",
-                alignSelf: "stretch",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-            >
-              <div className="player-info">
-                <p className="player-title">{selectedEpisodePlaying.title}</p>
-                <p className="player-pubdate">
-                  {selectedEpisodePlaying.pubDate}
-                </p>
-                <p className="player-description">
-                  {selectedEpisodePlaying.description}
-                </p>
-              </div>
-              <EpisodePlayer episode={selectedEpisodePlaying} />
-            </Box>
-          )}
-        </Box>
-      </AppBar>
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: "8rem",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="open sidebar"
+            onClick={toggleDrawer(true)}
+            edge="start"
+            sx={{ mr: 2, ml: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <h1>Ultimate Podcasting</h1>
+        </Toolbar>
+        {selectedEpisodePlaying && (
+          <EpisodePlayer episode={selectedEpisodePlaying} />
+        )}
+      </Box>
+    </AppBar>
   );
 }
 
